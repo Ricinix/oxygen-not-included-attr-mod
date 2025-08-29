7f045da65ed1ddb738ef85cd423c05087f34a4aa
Your Name <you@example.com>
2025-08-29 20:52:32 +0800

    doc: README

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# oni_attr_mod
+# oni_attr_mod (缺氧属性修改)
+
+## 模组介绍
+修改大部分属性，大幅降低游戏难度，适合新手使用。
+
+1. 标准仿生人无坏特性、每个特长为 1000，初始4个好特性
+2. 传送门无限打印
+3. 经验获取加成
+4. 学一个技能给的大幅士气奖励
+5. 运动服行走速度提升
+6. 储存上限提升
+7. 电量过载上限提升
+8. 储电量上限提升
+9. 电池耗电量降低
+10. 发电机发电量提升
+
+## Mod Intro
+This mod significantly reduces the difficulty of the game by modifying most attributes, making it suitable for beginners.
+
+1. Standard bionic duplicants have no bad traits, each specialty is 1000, and they start with 4 good traits.
+2. Portals can print infinitely.
+3. Experience gain is increased.
+4. Learning a skill provides a significant morale boost.
+5. Athletic suits increase walking speed.
+6. Storage limits are increased.
+7. Power overload limits are increased.
+8. Battery capacity limits are increased.
+9. Battery power consumption is reduced.
+10. Generator power output is increased.
--- oni_attr_mod/oni_patch.cs
+++ oni_attr_mod/oni_patch.cs
@@ -14,20 +14,7 @@ namespace oni_attr_mod
     public class Patches
     {
 
-        //[HarmonyPatch(typeof(ElectrolyzerConfig))]
-        //[HarmonyPatch("CreateBuildingDef")]
-        //public class ElectrolyzerConfig_CreateBuildingDef_Patch
-        //{
-        //    public static void Postfix(ref BuildingDef __result)
-        //    {
-        //        __result.Mass = new float[] { 200f, 50f };
-        //        __result.ConstructionTime = 30f;
-        //        __result.ExhaustKilowattsWhenActive = 0.5f;
-        //        __result.SelfHeatKilowattsWhenActive = 1f;
-        //    }
-        //}
-
-        // 传送门无限打印修改
+        // 传送门无限打印修改(Unlimited printing and modification of the portal)
         [HarmonyPatch(typeof(Immigration))]
         [HarmonyPatch("Sim200ms")]
         public class Immigration_Sim200ms_Patch
@@ -39,7 +26,7 @@ namespace oni_attr_mod
             }
         }
 
-        // 修改小人兴趣数量和数值属性
+        // 修改小人兴趣数量和数值属性(Modify the number and value attributes of the little people's interests)
         [HarmonyPatch(typeof(MinionStartingStats))]
         [HarmonyPatch("GenerateAptitudes")]
         public class MinionStartingStats_GenerateAptitudes_Patch
@@ -68,7 +55,7 @@ namespace oni_attr_mod
             }
         }
 
-        // 修改小人兴趣数量和数值属性
+        // 修改小人兴趣数量和数值属性(Modify the number and value attributes of the little people's interests)
         [HarmonyPatch(typeof(Db))]
         [HarmonyPatch("Initialize")]
         public class Db_Initialize_Patch
@@ -79,26 +66,26 @@ namespace oni_attr_mod
                     1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000,
                     1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000
                 };
-                // 没有坏习惯
+                // 没有坏习惯(No bad habits)
                 TUNING.DUPLICANTSTATS.BADTRAITS = TUNING.DUPLICANTSTATS.GOODTRAITS;
-                // 经验获取加成
+                // 经验获取加成(Experience gain bonus)
                 TUNING.SKILLS.PASSIVE_EXPERIENCE_PORTION = 500f;
-                // 学一个技能给的士气奖励修改
+                // 学一个技能给的士气奖励修改(Modify the morale reward for learning a skill)
                 TUNING.DUPLICANTSTATS.APTITUDE_BONUS = 1000;
-                // 航天运动服行走速度提升
+                // 航天运动服行走速度提升(Atmosuit walking speed boost)
                 TUNING.EQUIPMENT.SUITS.ATMOSUIT_ATHLETICS = 6;
-                // 铅服行走速度提升
+                // 铅服行走速度提升(Lead suit walking speed boost)
                 TUNING.EQUIPMENT.SUITS.LEADSUIT_ATHLETICS = 8;
-                // 氧气面罩行走速度提升
+                // 氧气面罩行走速度提升(Oxygen mask walking speed boost)
                 TUNING.EQUIPMENT.SUITS.OXYGEN_MASK_ATHLETICS = 2;
-                // 游戏原始储存上限
+                // 游戏原始储存上限(Original game storage limit)
                 PrimaryElement.MAX_MASS = 1000000000f;
 
                 Debug.Log($"[ONI_ATTR_MOD] APTITUDE_ATTRIBUTE_BONUSES patched in Db.Initialize. Length: {TUNING.DUPLICANTSTATS.APTITUDE_ATTRIBUTE_BONUSES.Length}");
             }
         }
 
-        // 储存箱100倍修改
+        // 储存箱100倍修改(Storage box 100x modification)
         [HarmonyPatch(typeof(Storage))]
         [HarmonyPatch(MethodType.Constructor)]
         public class Storage_Constructor_Patch
@@ -109,7 +96,7 @@ namespace oni_attr_mod
             }
         }
 
-        // 储气罐100倍修改
+        // 储气罐100倍修改(Gas reservoir 100x modification)
         [HarmonyPatch(typeof(GasReservoirConfig))]
         [HarmonyPatch("ConfigureBuildingTemplate")]
         public class GasReservoirConfig_ConfigureBuildingTemplate_Patch
@@ -123,7 +110,7 @@ namespace oni_attr_mod
             }
         }
 
-        // 储气罐100倍修改
+        // 储气罐100倍修改(Liquid reservoir 100x modification)
         [HarmonyPatch(typeof(LiquidReservoirConfig))]
         [HarmonyPatch("ConfigureBuildingTemplate")]
         public class LiquidReservoirConfig_ConfigureBuildingTemplate_Patch
@@ -137,7 +124,7 @@ namespace oni_attr_mod
             }
         }
 
-        // 食物盒100倍修改
+        // 食物盒100倍修改(Ration box 100x modification)
         [HarmonyPatch(typeof(RationBoxConfig))]
         [HarmonyPatch("ConfigureBuildingTemplate")]
         public class RationBoxConfig_ConfigureBuildingTemplate_Patch
@@ -149,7 +136,7 @@ namespace oni_attr_mod
             }
         }
 
-        // 冰箱1000倍修改
+        // 冰箱1000倍修改(Refrigerator 1000x modification)
         [HarmonyPatch(typeof(RefrigeratorConfig))]
         [HarmonyPatch("DoPostConfigureComplete")]
         public class RefrigeratorConfig_DoPostConfigureComplete_Patch
@@ -161,7 +148,7 @@ namespace oni_attr_mod
             }
         }
 
-        // 电池电量100倍 and 耗电量修改
+        // 电池电量100倍 and 耗电量修改(Battery capacity 100x and power consumption modification)
         [HarmonyPatch(typeof(BatteryConfig))]
         [HarmonyPatch("DoPostConfigureComplete")]
         public class BatteryConfig_DoPostConfigureComplete_Patch
@@ -174,7 +161,7 @@ namespace oni_attr_mod
             }
         }
 
-        // 电线负载修改
+        // 电线负载修改(Wire load modification)
         [HarmonyPatch(typeof(Wire))]
         [HarmonyPatch("GetMaxWattageAsFloat")]
         public class Wire_GetMaxWattageAsFloat_Patch
@@ -185,7 +172,7 @@ namespace oni_attr_mod
             }
         }
 
-        // 发电机发电100倍
+        // 发电机发电100倍(Generator 100x modification)
         [HarmonyPatch(typeof(GeneratorConfig))]
         [HarmonyPatch("CreateBuildingDef")]
         public class GeneratorConfig_CreateBuildingDef_Patch
@@ -197,7 +184,7 @@ namespace oni_attr_mod
             }
         }
 
-        // 人力发电机发电100倍
+        // 人力发电机发电100倍(Manual generator 100x modification)
         [HarmonyPatch(typeof(ManualGeneratorConfig))]
         [HarmonyPatch("CreateBuildingDef")]
         public class ManualGeneratorConfig_CreateBuildingDef_Patch
@@ -209,7 +196,7 @@ namespace oni_attr_mod
             }
         }
 
-        // 天燃气发电机发电100倍
+        // 天燃气发电机发电100倍(Methane generator 100x modification)
         [HarmonyPatch(typeof(MethaneGeneratorConfig))]
         [HarmonyPatch("CreateBuildingDef")]
         public class MethaneGeneratorConfig_CreateBuildingDef_Patch
@@ -221,7 +208,7 @@ namespace oni_attr_mod
             }
         }
 
-        // 氢气发电机发电100倍
+        // 氢气发电机发电100倍(Hydrogen generator 100x modification)
         [HarmonyPatch(typeof(HydrogenGeneratorConfig))]
         [HarmonyPatch("CreateBuildingDef")]
         public class HydrogenGeneratorConfig_CreateBuildingDef_Patch
@@ -233,7 +220,7 @@ namespace oni_attr_mod
             }
         }
 
-        // 石油发电机发电100倍
+        // 石油发电机发电100倍(Petroleum generator 100x modification)
         [HarmonyPatch(typeof(PetroleumGeneratorConfig))]
         [HarmonyPatch("CreateBuildingDef")]
         public class PetroleumGeneratorConfig_CreateBuildingDef_Patch
@@ -245,7 +232,7 @@ namespace oni_attr_mod
             }
         }
 
-        // 木燃发电机发电100倍
+        // 木燃发电机发电100倍(Wood gas generator 100x modification)
         [HarmonyPatch(typeof(WoodGasGeneratorConfig))]
         [HarmonyPatch("CreateBuildingDef")]
         public class WoodGasGeneratorConfig_CreateBuildingDef_Patch
@@ -257,7 +244,7 @@ namespace oni_attr_mod
             }
         }
 
-        // 蒸汽发电机发电100倍
+        // 蒸汽发电机发电100倍(Steam generator 100x modification)
         [HarmonyPatch(typeof(StaterpillarGeneratorConfig))]
         [HarmonyPatch("CreateBuildingDef")]
         public class StaterpillarGeneratorConfig_CreateBuildingDef_Patch
